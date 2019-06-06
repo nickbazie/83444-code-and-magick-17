e6e406e60b15c87969e21b207ccc7ba00b5495fd
nickbazie <nico@alwaysmarbella.com>
2019-06-06 22:45:36 +0200

    fix (JS) change vars to const vars

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,13 @@
 'use strict';
 var canvasPositionX = 100;
 var canvasPositionY = 10;
-var canvasWidth = 420;
-var canvasHeight = 270;
+var CANVAS_WIDTH = 420;
+var CANVAS_HEIGHT = 270;
 
 window.renderStatistics = function(ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; //canvas shadow
-  ctx.fillRect(canvasPositionX + 10, canvasPositionY + 10, canvasWidth, canvasHeight)
+  ctx.fillRect(canvasPositionX + 10, canvasPositionY + 10, CANVAS_WIDTH, CANVAS_HEIGHT)
 
   ctx.fillStyle = '#fff'; //canvas body
-  ctx.fillRect(canvasPositionX, canvasPositionY, canvasWidth, canvasHeight);
+  ctx.fillRect(canvasPositionX, canvasPositionY, CANVAS_WIDTH, CANVAS_HEIGHT);
 };
